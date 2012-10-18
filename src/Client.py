b3ee05d0ec954d0dd2882ffb1e1d2a6d679f43e1
Ben Grabham <bsg110@ic.ac.uk>
2012-10-18 17:34:57 +0100

    Added relevant submodules, added a web server and console client and updated the gitignore to include emacs temporary files

---
+++ src/Client.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import cv2
+import sys
+from VideoWrapper import VideoWrapper, SplitType
+
+class Client:
+  # Initialise the client class.
+  # The video can be of type string, cv image or video wrapper.
+  def __init__(self, video, splitType):
+    self.splitType = splitType
+    if isinstance(video, VideoWrapper):
+      self.video = video
+    elif isinstance(video, type(cv2.VideoCapture())):
+      self.video = VideoWrapper(video)
+    elif isinstance(video, str):
+      self.video = VideoWrapper(cv2.VideoCapture(video))
+    else:
+      assert 0, ("Unknown type of video parameter (%s)" % type(video))
+
+  # Run the correct functions based on what the client wants.
+  def run(self):
+    return self.video.getSegments(self.splitType)
+
+# If we call this file directly, convert the video and show it onscreen.
+if __name__ == "__main__":
+  # If we haven't got enough arguments, display usage instructions.
+  if len(sys.argv) < 3:
+    print("USAGE: %s FILENAME SPLIT_TYPE" % sys.argv[0])
+    sys.exit()
+
+  # Convert the video
+  segments = Client(sys.argv[1], int(sys.argv[2])).run()
+
+  # Show it onscreen
+  for i, _ in enumerate(segments):
+    cv2.namedWindow("Video renderer %d" % (i +1))
+
+  # Loop till ESC pressed
+  while((cv2.waitKey(33) & 0xFF) != 27):
+    for (i, segment) in enumerate(segments):
+      _, img = segment.video.read()
+      cv2.imshow("Video renderer %d" % (i + 1), img)
+
+  cv2.destroyAllWindows()
---
+++ src/WebServer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import web
+import tempfile
+import VideoWrapper
+from Client import WebClient
+from web.contrib.template import render_jinja
+
+# We only want the main index
+urls = (
+  '/', 'Index'
+)
+
+# Create an app and render context
+app = web.application(urls, globals())
+render = render_jinja("src/assets", encoding = "utf-8")
+
+# The main web class
+class Index:
+  # On GET, we just display the jinja page
+  def GET(self):
+    print VideoWrapper.SplitType.__dict__
+    return render.index(SplitType = VideoWrapper.SplitType)
+
+  # On POST, we create a client
+  def POST(self):
+    file = tempfile.NamedTemporaryFile()
+    file.write(web.input(video={})['video'].value)
+    file.flush()
+    return Client(file.name, None)
+
+# If we run this file, start the web server
+if __name__ == "__main__":
+    app.run()
