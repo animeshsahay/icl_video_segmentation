9e910a2de05af3418509d10c7dc499b4d76c6ae0
Ben Grabham <bsg110@ic.ac.uk>
2012-10-24 18:57:53 +0100

    Only add frames which have faces in getFaces

--- src/VideoWrapper.py
+++ src/VideoWrapper.py
@@ -104,7 +104,8 @@ class VideoWrapper:
             img_copy = cvtColor(frame, COLOR_BGR2GRAY)
             img_copy = equalizeHist(img_copy)
             rects = cascade.detectMultiScale(img_copy, minNeighbors = 11, flags = cv.CV_HAAR_SCALE_IMAGE, minSize = (30, 30))
-            faces.append((frameNo, rects))
+            if len(rects) > 0:
+                faces.append((frameNo, rects))
             frameNo += 1
         return faces
 
