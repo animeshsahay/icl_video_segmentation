411d13d57d1459c7495836322156ecede8c7f3fd
Agnieszka Szefer <aga@dyn1203-52.wlan.ic.ac.uk>
2012-11-08 20:30:42 +0000

    added funtions for PCA; problem when running on mac

--- src/FaceClustering.py
+++ src/FaceClustering.py
@@ -130,19 +130,114 @@ def writeFace(f, index):
 #        return V,S,mean_X
 #
 #
+
+'''
+def read_images(path, sz=None):
+    c=0
+    X,y = [], []
+    for dirname , dirnames , filenames in os.walk(path):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname , subdirname)
+            for filename in os.listdir(subject_path):
+                try:
+                    im = Image.open(os.path.join(subject_path , filename))
+                    im = im.convert("L")
+                    # resize to given size (if given)
+                    if (sz is not None):
+                        im = im.resize(sz, Image.ANTIALIAS)
+                        X.append(np.asarray(im, dtype=np.uint8))
+                        y.append(c)
+                except IOError:
+                    print "I/O error({0}): {1}".format(errno, strerror)
+                except:
+                    print "Unexpected error:", sys.exc_info()[0]
+                    raise
+            c = c+1
+    return [X,y]
+
+
+def asRowMatrix(X):
+    if len(X) == 0:
+        return np.array([])
+    mat = np.empty((0, X[0].size), dtype=X[0].dtype)
+    for row in X:
+        mat = np.vstack((mat, np.asarray(row).reshape(1,-1)))
+    return mat
+
+def pca(X, y, num_components=0):
+    [n,d] = X.shape
+    if (num_components <= 0) or (num_components >n):
+        num_components = n
+    mu = X.mean(axis=0)
+    X = X - mu
+    if n>d:
+        C = np.dot(X.T,X)
+        [eigenvalues ,eigenvectors] = np.linalg.eigh(C)
+    else:
+        C = np.dot(X,X.T)
+        [eigenvalues ,eigenvectors] = np.linalg.eigh(C)
+        eigenvectors = np.dot(X.T,eigenvectors)
+        for i in xrange(n):
+            eigenvectors[:,i] = eigenvectors[:,i]/np.linalg.norm(eigenvectors[:,i])
+    # or simply perform an economy size decomposition
+    # eigenvectors , eigenvalues , variance = np.linalg.svd(X.T, full_matrices=False)
+    # sort eigenvectors descending by their eigenvalue
+    idx = np.argsort(-eigenvalues)
+    eigenvalues = eigenvalues[idx]
+    eigenvectors = eigenvectors[:,idx]
+    # select only num_components
+    eigenvalues = eigenvalues[0:num_components].copy() 
+    eigenvectors = eigenvectors[:,0:num_components].copy()
+    return [eigenvalues , eigenvectors , mu]
+
+def project(W, X, mu=None):
+    if mu is None:
+        return np.dot(X,W)
+    return np.dot(X - mu, W)
+
+'''
+
+
 if __name__ == "__main__":
     faces = readFaces(VideoWrapper(VideoCapture(sys.argv[1]), 2650, 2800))
+    #[X,y] = read_images(")
+    
+    
+    
+    
+    
+    
+    
+    
+    '''
+    mean, eigenvectors = cv2.PCACompute(np.array([faces[20].flatten()]))
+    
+    f = cv2.equalizeHist(faces[0])
+    cv2.normalize(f, f, 0, 255, cv2.NORM_MINMAX)
+    vec = f.reshape(1, f.shape[0]*f.shape[1])
+    projection = cv2.PCAProject(vec, mean, eigenvectors)
+    '''
+    
+    '''
     data = np.array([f.flatten() for f in faces])
     rows = len(faces)
     #data = cv.fromarray(data)
     #data.reshape(rows, cols)
     #print data[:3]
-
-    mean, pcaCompute = cv2.PCACompute(data)
-    pcaProject = cv2.PCAProject(data, mean, pcaCompute)
-    print pcaProject
-    imshow("img", pcaProject)
+    
+    np.reshape(mean, (len(data[0]), 1))
+    normalize(mean, mean, 0, 255, NORM_MINMAX);
+    imshow("mean", mean)
     waitKey(0)
+    '''
+    
+    #print mean
+    #pcaProject = cv2.PCAProject(data, mean, pcaCompute)
+    #comps = np.dot(data.transpose(), pcaProject)
+#print(comps)
+#   imshow("img", comps)
+    #imshow("mean", mean)
+#   waitKey(0)
     
 
     #sim = clusterFaces(faces)
