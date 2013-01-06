7d1b1b98bbdd7a2c7d012c2ccdf5e44b261406b6
Ben Grabham <bsg110@ic.ac.uk>
2013-01-06 22:47:49 +0000

    Removed some debug code and allowed the location to change

--- runParallelValidate.d
+++ runParallelValidate.d
@@ -1,24 +1,33 @@
 import std.concurrency : thisTid, send, receive, Tid, spawn;
 import std.container : SList;
+import std.getopt  : getopt, config;
 import std.process : shell, system;
 import std.stdio : writefln, format, chomp;
 
 auto videos = ["res/hungergames-720p.mp4", "res/skyfall.mp4", "res/tangled.mp4"];
 
-void main() {
-  auto compTid = spawn(&distributeComputers);
+void main(string[] args) {
+  if(args.length != 2) {
+    writefln("USAGE: %s <location of repo>");
+    return;
+  }
+
+  auto location = args[1];
+
+  auto compTid = spawn(&distributeComputers, thisTid);
 
   Tid[] threads;
   foreach(video; videos)
-    threads ~= spawn(&runVideo, thisTid, compTid, video);
+    threads ~= spawn(&runVideo, thisTid, compTid, location, video);
 
   foreach(i; 0..threads.length)
-    receive((string s) { writefln("Video %s is done", s); });
+    receive((string s) { writefln("Video %s is done", s); },
+            (bool b) { throw new Exception("Out of computers!"); });
 
   send(compTid, true);
 }
 
-void distributeComputers() {
+void distributeComputers(Tid masterTid) {
   SList!string comps;
 
   foreach(i; 1..25)
@@ -26,11 +35,11 @@ void distributeComputers() {
 
   bool done = false;
   while(!done)
-    receive((Tid tid) { writefln("Sending.."); send(tid, comps.removeAny()); },
+    receive((Tid tid) { if(comps.empty()) send(masterTid, false); send(tid, comps.removeAny()); },
             (bool s) { done = s; });
 }
 
-void runVideo(Tid masterTid, Tid compTid, string video) {
+void runVideo(Tid masterTid, Tid compTid, string location, string video) {
   writefln(video);
   bool done = false;
   string comp = "";
@@ -41,7 +50,7 @@ void runVideo(Tid masterTid, Tid compTid, string video) {
     writefln("Trying on %s", comp);
     auto output = system("ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
                          ~ comp
-                         ~ " \"cd Development/Python/icl_video_segmentation && make validate ARGS='-v " ~ video ~ " -f 10' 2>/dev/null > " ~ comp ~ ".output\"");
+                         ~ " \"cd '" ~ location ~ "' && make validate ARGS='-v " ~ video ~ "' 2>/dev/null > " ~ comp ~ ".output\"");
     done = output == 0;
   }
 
